7ee1aec7f34dbc9f7c22fad90be4750f64d7415f
AndyMtz04 <andres.mtz044@gmail.com>
2016-01-01 20:20:57 -0600

    Add function to parse austincc jobs and pep8 code

--- jobreporter.py
+++ jobreporter.py
@@ -1,18 +1,27 @@
+import json
+
 import csv
+
 import os
+
 import smtplib
+
 from datetime import datetime
+
 from urllib.request import urlopen
+
 from bs4 import BeautifulSoup
 
 
 class JobReport(object):
+
     def __init__(self, file_name, file_path):
         self.file_name = file_name
         self.file_path = file_path
         self.complete_path = self.file_path + self.file_name
 
     def parse_results(self, url):
+        """Function parses job postings from a cragslist job section."""
         results = []
         count = 0
         html = urlopen(url).read()
@@ -24,19 +33,42 @@ class JobReport(object):
                 post_url = "https://austin.craigslist.org" + row.a["href"]
                 post_date = row.find("time").get("datetime")
                 post_title = row.find_all("a")[1].get_text()
-                results.append({"url": post_url, "post_date": post_date, "post_title": post_title})
+                results.append({"url": post_url, "post_date": post_date,
+                                "post_title": post_title})
             else:
                 break
         return results
 
+    def austincc_parse(self, url):
+        """Function parses job data into a list from
+        http://sites.austincc.edu/jobs/jobs with import.io's api.
+        """
+        results = []
+        html = urlopen(url)
+        content = html.read().decode(html.headers.get_content_charset())
+        dict = json.loads(content)
+        for x in dict["results"]:
+            job_title = x["linemajor_link/_text"]
+            job_link = x["linemajor_link"]
+            job_time = x["subsmall_value_2"]
+            job_company = x["subsmall_value_1"]
+            results.append({"job_title": job_title, "job_link": job_link,
+                            "job_time": job_time, "job_company": job_company})
+        return results
+
     def write_results(self, results):
+        """Function writes results to a specified csv file"""
         fields = list(results[0].keys())
         with open(self.complete_path, "w") as file:
-            dw = csv.DictWriter(file, fieldnames=fields, delimiter="|", lineterminator="\n")
+            dw = csv.DictWriter(file, fieldnames=fields, delimiter="|",
+                                lineterminator="\n")
             dw.writer.writerow(dw.fieldnames)
             dw.writerows(results)
 
     def has_new_records(self, results):
+        """Function compares results with stored results to determine
+         if new jobs have been posted.
+         """
         current_posts = [x["url"] for x in results]
         if not os.path.exists(self.complete_path):
             with open(self.complete_path, "w") as new_file:
@@ -54,9 +86,13 @@ class JobReport(object):
         return is_new
 
     def send_text(self, username, password, phone_number, msg):
+        """Function sends text message to specified recipient using
+        gmail.
+        """
         from_address = "Craigslist Checker"
         to_address = phone_number + "@tmomail.net"
-        msg = "From: {0}\r\nTo: {1}\r\n\r\n{2}".format(from_address, to_address, msg)
+        msg = "From: {0}\r\nTo: {1}\r\n\r\n{2}".format(from_address,
+                                                       to_address, msg)
         server = smtplib.SMTP("smtp.gmail.com", 587)
         server.starttls()
         server.login(username, password)
@@ -67,6 +103,7 @@ class JobReport(object):
         return datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S")
 
     def new_job(self, results):
+        """Function extracts new job titles and urls from results."""
         final_results = []
         with open(self.complete_path, "r") as file:
             reader = csv.DictReader(file, delimiter="|")
